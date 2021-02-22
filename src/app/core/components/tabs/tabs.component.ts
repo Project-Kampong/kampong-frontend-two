1a0b315f1f45ef08b4ad29b008c7da2ec87b3cd7
shengliang <ahliang51@hotmail.com>
2021-02-23 00:43:53 +0800

    added tab

---
+++ src/app/core/components/tabs/tabs.component.ts
@@ -0,0 +1,31 @@
+import { AfterContentInit, Component, ContentChildren, Input, QueryList } from '@angular/core';
+import { TabComponent } from './tab/tab.component';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss'],
+})
+export class TabsComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = {} as QueryList<TabComponent>;
+  @Input('id') listingId: String = '' as string;
+
+  // contentChildren are set
+  ngAfterContentInit() {
+    // get all active tabs
+    let activeTabs = this.tabs.filter((tab: TabComponent) => tab.active);
+
+    // if there is no active tab set, activate the first
+    if (activeTabs.length === 0) {
+      this.selectTab(this.tabs.first);
+    }
+  }
+
+  selectTab(tab: any) {
+    // deactivate all tabs
+    this.tabs.toArray().forEach((tab) => (tab.active = false));
+
+    // activate the tab the user has clicked on.
+    tab.active = true;
+  }
+}
